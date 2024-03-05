a5a5d951acf7aa093705a784426709ad8c4d69eb
= mariafadilla15 <= mariafadilla15@gmail.com>
2024-03-05 22:41:54 +0700

    Week 02: belajar react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
+function MyButton() {
+  return (
+    <button>Saya adalah tombol</button>
+  );
+}
+
 function MyTextNimName() {
   return (
     <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -145,6 +151,15 @@ function MyPage() {
     </main >
   )
 }
+
+// export default function MyApp() {
+//   return (
+//     <div>
+//       <h1>Selamat datang di aplikasi saya</h1>
+//       <MyButton />
+//     </div>
+//   );
+// }
 
 export default function Home() {
   return (
